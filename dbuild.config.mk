3805e1ef67c4cadc1229dbb3eb683a377637330e
Carl Brando <cbrando@umd.edu>
2019-01-14 00:38:52 -0500

    first commit

---
+++ dbuild.config.mk
@@ -0,0 +1,8 @@
+
+CFLAGS += -march=armv6z -g -Wall -Wextra
+CFLAGS += -I $(BASE)FreeRTOS/Source/portable/GCC/RaspberryPi/
+CFLAGS += -I $(BASE)FreeRTOS/Source/include/
+CFLAGS += -I $(BASE)Demo/Drivers/
+CFLAGS += -I $(BASE)uspi/include
+
+TOOLCHAIN=arm-none-eabi-
